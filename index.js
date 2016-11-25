5ac4f7df2bd5af63c789b71c8be15d0558430b9d
slyguy <theslyguy@icloud.com>
2016-11-25 16:23:41 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,48 +1,71 @@
 var http = require("http");
 var express = require("express");
 
-const ImagesClient = require('google-images');
-//const googleIms = require('google-ims');
-const got = require('got');
 var google = require('googleapis');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var moment = require("moment");
 
-var API_KEY = 'AIzaSyCdxl0vCH_vXT-ijYqADbEO5WXFfMHHtYQ';
+const API_KEY = 'AIzaSyCdxl0vCH_vXT-ijYqADbEO5WXFfMHHtYQ';
+const CSE_ID = '007375434282591706073:jvjnxmqnnjo';
+const db_url = process.env.MONGOLAB_URI;
 
 var app = express();
+app.set('views', __dirname + '/views');
+app.set("view engine", "pug");
+app.use(express.static(__dirname + '/public'));
+
 app.get("/api/imagesearch/:s_param", function(request, response){
   let s_param = request.params.s_param;
   let offset = request.query.offset || 1;
 
+  MongoClient.connect(db_url, function(err, db) {
+    assert.equal(null, err);
+    var formattedDate = moment(new Date()).format('MMMM DD, YYYY hh:mm:ss');
+    db.collection("imagesearch_log").insertOne({ term: s_param, when: formattedDate }, function(err2, value){
+      console.log("Search " + s_param + " saved");
+      db.close();
+    });
+  });
+
   var customsearch = google.customsearch({ version: 'v1', auth: API_KEY });
   params = {
-    cx: '007375434282591706073:jvjnxmqnnjo',
-    q: 'cats',
+    cx: CSE_ID,
+    q: s_param,
     searchType: 'image',
-    start: 1
+    start: offset
   };
-  customsearch.cse.list(params, function(err, result){
+
+  let result = [];
+  customsearch.cse.list(params, function(err, searchResult){
     if (err) {
-      response.end('Encountered error 2', err);
+      response.end('Encountered error', err);
     } else {
+      searchResult.items.forEach(function(item){
+        result.push({url: item.link, snippet: item.snippet, thumbnail: item.image.thumbnailLink, context: item.image.contextLink });
+      });
       response.end(JSON.stringify(result, null, 4));
     }
   });
-
-
-  /*let client = new ImagesClient('007375434282591706073:jvjnxmqnnjo', 'AIzaSyCdxl0vCH_vXT-ijYqADbEO5WXFfMHHtYQ');
-  client.search(s_param)
-      .then(function (images) {
-        let result = [];
-        console.log(images);
-        images.forEach(function(image){
-          result.push({url: image.url, thumbnail: image.thumbnail.url });
-        });
-        //response.end(JSON.stringify(result, null, 4));
-        console.log(JSON.stringify(images, null, 4));
-        response.end(JSON.stringify(images, null, 4));
-      });
-*/
 });
+
+app.get("/api/latest", function(request, response){
+  MongoClient.connect(db_url, function(err, db) {
+    assert.equal(null, err);
+    db.collection("imagesearch_log").find({}, function(err2, result){
+      if (err2) {
+        response.end('Encountered error retrieving lastest image search: ', err2);
+      } else {
+        response.end(JSON.stringify(result, null, 4));
+      }
+      db.close();
+    });
+  });
+});
+
+app.get("/", function(req,res){
+  res.render('index');
+})
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 8080);
